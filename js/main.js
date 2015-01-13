d2220b33af53b91ed0691427740d1e663dace4ab
shrikarch <shrikar.code@gmail.com>
2015-01-13 22:52:28 +0530

    slider

--- js/main.js
+++ js/main.js
@@ -10,23 +10,23 @@ $(document).ready(function(){
     //$('#wrapper').hide();
     //askPassword();
     $('.opaque').css('opacity','1');
+    $('.carousel').carousel({
+        interval: 3000,
+        pause: "false"
+    });
+     $('.nav-list li a').smoothScroll({
+        speed: 1500
+    });
 
-
-
-    /* Every time the window is scrolled ... */
+    /* Appear */
     $(window).scroll( function(){
-
         /* Check the location of each desired element */
         $('.hideme').each( function(i){
-
             var bottom_of_object = $(this).position().top + $(this).outerHeight() - 100;
             var bottom_of_window = $(window).scrollTop() + $(window).height();
-
             /* If the object is completely visible in the window, fade it it */
             if( bottom_of_window > bottom_of_object ){
-
                 $(this).animate({'opacity':'1'},500);
-
             }
 
         });
