58e2cb51085bf3bbd1f2dd5e0b6854071bef7a24
shrikarch <shrikar.code@gmail.com>
2015-01-20 02:06:22 +0530

    chomu

--- js/main.js
+++ js/main.js
@@ -7,14 +7,14 @@ function askPassword(){
     }
 };
 $(document).ready(function(){
-    $('#wrapper').hide();
-    askPassword();
+    //$('#wrapper').hide();
+    //askPassword();
     $('.opaque').css('opacity','1');
     $('.carousel').carousel({
         interval: 3000,
         pause: "false"
     });
-     $('.nav-list li a').smoothScroll({
+    $('.nav-list li a').smoothScroll({
         speed: 1500
     });
 
@@ -32,4 +32,45 @@ $(document).ready(function(){
         });
 
     });
+
+    var form = $('#ajax-contact');
+
+    // Get the messages div.
+    var formMessages = $('#form-messages');
+
+    // Set up an event listener for the contact form.
+    $(form).submit(function(event) {
+        // Stop the browser from submitting the form.
+        event.preventDefault();
+        var formData = $(form).serialize();
+        $.ajax({
+            type: 'POST',
+            url: $(form).attr('action'),
+            data: formData
+        }).done(function(response) {
+            // Make sure that the formMessages div has the 'success' class.
+            $(formMessages).removeClass('error');
+            $(formMessages).addClass('success');
+
+            // Set the message text.
+            $(formMessages).text(response);
+
+            // Clear the form.
+            $('#name').val('');
+            $('#email').val('');
+            $('#message').val('');
+        }).fail(function(data) {
+            // Make sure that the formMessages div has the 'error' class.
+            $(formMessages).removeClass('success');
+            $(formMessages).addClass('error');
+
+            // Set the message text.
+            if (data.responseText !== '') {
+                $(formMessages).text(data.responseText);
+            } else {
+                $(formMessages).text('Oops! An error occured and your message could not be sent.');
+            }
+        });
+    });
+
 });
