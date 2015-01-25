37e98a6c93fe5414491636bc07106b9c172ab436
shrikarch <shrikar.code@gmail.com>
2015-01-25 19:56:20 +0530

    nom

---
+++ mail.php
@@ -0,0 +1,11 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "shrikarz@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+if(mail($recipient, $subject, $formcontent, $mailheader)){
+    echo "Thank You!";
+} or die("Error!");
+?>
