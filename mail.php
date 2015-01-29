ccd3b0996425eef5df6ae7ac3deb973378cefae2
shrikarch <shrikar.code@gmail.com>
2015-01-29 14:28:05 +0530

    rog

--- mail.php
+++ mail.php
@@ -1,17 +1,39 @@
 <?php
-require "phpmailer/class.phpmailer.php"; //include phpmailer class
-require "phpmailer/class.smtp.php";
+require 'phpmailer/PHPMailerAutoload.php';
+$mail = new PHPMailer;
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "shrikarz@gmail.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-if(mail($recipient, $subject, $formcontent, $mailheader)){
-    echo "Thank You!";
-} else{
-    echo "something went wrong";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Construct the Google verification API request link.
+    $params = array();
+    $params['secret'] = 'Your secret key here.'; // Secret key
+    if (!empty($_POST) && isset($_POST['g-recaptcha-response'])) {
+        $params['response'] = urlencode($_POST['g-recaptcha-response']);
+    }
+    $params['remoteip'] = $_SERVER['REMOTE_ADDR'];
+
+    $params_string = http_build_query($params);
+    $requestURL = 'https://www.google.com/recaptcha/api/siteverify?' . $params_string;
+
+    // Get cURL resource
+    $curl = curl_init();
+
+    // Set some options
+    curl_setopt_array($curl, array(
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => $requestURL,
+    ));
+
+    // Send the request
+    $response = curl_exec($curl);
+    // Close request to clear up some resources
+    curl_close($curl);
+
+    $response = @json_decode($response, true);
+
+    if ($response["success"] == true) {
+        echo '<h3 class="alert alert-success">Login Successful</h3>';
+    } else {
+        echo '<h3 class="alert alert-danger">Login failed</h3>';
+    }
 }
 ?>
