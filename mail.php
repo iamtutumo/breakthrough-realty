39efb1d967653d270ecee91d4c9dfd47138d603e
shrikarch <shrikar.code@gmail.com>
2015-01-29 16:16:09 +0530

    sff

--- mail.php
+++ mail.php
@@ -52,7 +52,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         $mail->From = 'sitemail@breakthroughgroup.in';
         $mail->FromName = 'BT mail';
-        $mail->addAddress('btgroup.companies.com');     // Add a recipient
+        $mail->addAddress('shrikarz@gmail.com');     // Add a recipient
         //$mail->addAddress('ellen@example.com');               // Name is optional
         $mail->addReplyTo("$email");
         //$mail->addCC('cc@example.com');
